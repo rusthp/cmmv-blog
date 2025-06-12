70177200719f5844987fefc02046973642e95495
rusthp <allyson.f.m@hotmail.com>
2025-06-12 00:57:39 -0400

    camp

--- apps/web/src/theme-proplaynews/views/PagePost.vue
+++ apps/web/src/theme-proplaynews/views/PagePost.vue
@@ -1369,6 +1369,7 @@ const sidebarLeftAdContainer = ref(null);
 .line-clamp-2 {
     display: -webkit-box;
     -webkit-line-clamp: 2;
+    line-clamp: 2;
     -webkit-box-orient: vertical;
     overflow: hidden;
 }
